ed39207dc52f26dbf3394e1533632218c488b08c
Nick Kozlov <22479658+EnoRage@users.noreply.github.com>
2023-07-14 21:35:41 +0200

    chore: patch pkg.json

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,6 @@ import {NetworkEnum} from "@1inch/fusion-sdk";
 export const pk = process.env.PK || '';
 export const ethNetworkRPC = process.env.ETH_NETWORK_RPC || 'https://eth.llamarpc.com';
 
-
 export const OneInchToken = '0x111111111117dC0aa78b770fA6A738034120C302';
 export const WETH_Token = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 export const OneInchRouter = '0x1111111254EEB25477B68fb85Ed929f73A960582';
